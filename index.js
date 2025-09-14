14975f583480c2e2882fbfea7a258386059d2d65
MaidanaThiago <maidana15t@gmail.com>
2025-09-14 18:52:08 -0300

    Mi portfolio

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 document.querySelectorAll(".Download").forEach(btn => {
-    btn.addEventListener("click", () => {
-      const link = document.createElement("a");
-      link.href = "CI.pdf";                       // ruta al archivo
-      link.download = "CV_Thiago_Maidana.pdf";    // nombre al guardar
-      document.body.appendChild(link);            // necesario en algunos navegadores
-      link.click();
-      link.remove();
-    });
+  btn.addEventListener("click", () => {
+    const link = document.createElement("a");
+    link.href = "CV/CVThiagoMaidana.pdf"; // Ruta correcta al archivo de tu CV
+    link.download = "CV_Thiago_Maidana.pdf"; // Nombre de descarga
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
   });
-  
+});
